e8af1a2a14fb8fb3d6488d15601a7e9357ac91ff
libangmin02 <libangmin02@meituan.com>
2026-03-23 00:35:08 +0800

    chore: remove leftover Python files

--- core/__init__.py
+++
@@ -1,8 +0,0 @@
-# core/__init__.py
-# 核心模块：消息、状态机、消息总线
-
-from .message import Message, MessageType
-from .state import StateMachine, State
-from .bus import MessageBus
-
-__all__ = ["Message", "MessageType", "StateMachine", "State", "MessageBus"]
